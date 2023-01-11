d47c06d759e0883caa7d02bb89fd90ace1bf521c
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-01-11 12:57:04 -0300

    Pacote Teste

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module com.example.exerciciosjava {
 
     opens com.example.exerciciosjava to javafx.fxml;
     exports com.example.exerciciosjava;
+    exports com.example.exerciciosjava.controller;
+    opens com.example.exerciciosjava.controller to javafx.fxml;
 }
