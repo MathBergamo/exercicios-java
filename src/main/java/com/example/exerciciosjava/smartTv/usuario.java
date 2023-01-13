8a8784b8cdd4ab00089c47be1ec5cb00ea26aedf
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-01-13 14:39:00 -0300

    smarTv exercicio

---
+++ src/main/java/com/example/exerciciosjava/smartTv/usuario.java
@@ -0,0 +1,22 @@
+package com.example.exerciciosjava.smartTv;
+
+public class usuario {
+    public static void main (String[]args) throws Exception{
+
+        SmartTv smartTv = new SmartTv();
+
+        if (!smartTv.ligada){
+            System.out.println("A TV está desligada");
+        }else{
+            System.out.println("A TV está ligada");
+        }
+        System.out.println("Canal: " +smartTv.canal);
+        System.out.println("Volume: " +smartTv.volume);
+
+        smartTv.ligar();
+        smartTv.diminuirCanal();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+
+    }
+}
