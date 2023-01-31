76a96e5a3a95894ec7ad3b0943ea094a635127d8
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-01-31 18:57:20 -0300

    Exercícios Array

---
+++ src/main/java/com/exerciciosjava/DIO/Exercicios/Arrays/Consoantes.java
@@ -0,0 +1,39 @@
+package com.exerciciosjava.DIO.Exercicios.Arrays;
+
+import java.util.Scanner;
+
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+public class Consoantes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String vetor[] = new String[5];
+        char letra;
+        int consoantes = 0, count = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Digite uma letra:");
+            letra = scan.next().charAt(0);
+            if (letra != 'a' && letra != 'e' && letra != 'i' && letra != 'o' && letra != 'u') {
+                consoantes++;
+            }
+        }
+        System.out.println("Temos o total de: " + consoantes + " consoantes");
+
+        consoantes = 0;
+
+        while (count < vetor.length) {
+            System.out.print("Digite uma letra: ");
+            letra = scan.next().charAt(0);
+            count++;
+            if (letra != 'a' && letra != 'e' && letra != 'i' && letra != 'o' && letra != 'u') {
+                consoantes++;
+            }
+        }
+        System.out.println("Temos o total de: " + consoantes + " consoantes");
+    }
+}
