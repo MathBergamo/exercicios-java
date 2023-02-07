f5143af8793c919dc6f6495722d252a734cac2ea
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-02-07 18:08:05 -0300

    Ex 22 sendo feito

--- src/main/java/com/exerciciosjava/Outros/PythonBrasil/EstruturaDeRepeticao/Ex22.java
+++ src/main/java/com/exerciciosjava/Outros/PythonBrasil/EstruturaDeRepeticao/Ex22.java
@@ -9,6 +9,26 @@ public class Ex22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = 0;
+        boolean primo = true;
 
+        System.out.println("Digite um número: ");
+        num = scanner.nextInt();
+
+        int array[] = new int[num];
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                array[i] = i;
+                primo = false;
+            }
+        }
+
+        if (primo) {
+            System.out.println("O número " + num + " é primo");
+        } else {
+            System.out.println("O número " + num + " não é primo");
+            System.out.println("Números divisíveis: " + array);
+        }
     }
 }
